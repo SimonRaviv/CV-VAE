ad080322d33950451c3a490b5aecff5efd6d3746
Simon Raviv <lionsimonlion@gmail.com>
2024-07-20 16:23:59 +0300

    Add time benchmark option
    Signed-off-by: Simon Raviv <lionsimonlion@gmail.com>

--- cvvae_inference_video.py
+++ cvvae_inference_video.py
@@ -1,22 +1,38 @@
 import torch
 import os
+import time
 
 from decord import VideoReader, cpu, gpu
 from einops import rearrange
 from torchvision.io import write_video
 from torchvision import transforms
 from fire import Fire
+from prettytable import PrettyTable
 
 from models.modeling_vae import CVVAEModel
 
 
-def main(vae_path, video_path, save_path, height=576, width=1024, frame_sample_rate=1, device='cuda', gpu_id=-1):
+def log_time_benchmark(ts_1, ts_2, ts_3, ts_4, ts_5):
+    table = PrettyTable()
+    table.field_names = ["Step", "Time (s)", "Time (ms)"]
+    table.add_row(["Read video", round(ts_2 - ts_1, 2), round((ts_2 - ts_1) * 1000, 2)])
+    table.add_row(["Encode video", round(ts_3 - ts_2, 2), round((ts_3 - ts_2) * 1000, 2)])
+    table.add_row(["Read & Encode video", round(ts_3 - ts_1, 2), round((ts_3 - ts_1) * 1000, 2)])
+    table.add_row(["Decode video", round(ts_4 - ts_3, 2), round((ts_4 - ts_3) * 1000, 2)])
+    table.add_row(["Write video", round(ts_5 - ts_4, 2), round((ts_5 - ts_4) * 1000, 2)])
+    print(table)
+
+
+def main(vae_path, video_path, save_path, height=576, width=1024,
+         frame_sample_rate=1, device='cuda', gpu_id=-1, timeit=False):
     vae3d = CVVAEModel.from_pretrained(vae_path, subfolder="vae3d", torch_dtype=torch.float16)
     vae3d.requires_grad_(False).to(device)
 
     transform = transforms.Compose([
         transforms.Resize(size=(height, width))
     ])
+
+    ts_1 = time.time()
 
     os.makedirs(os.path.dirname(save_path), exist_ok=True)
     video_reader_context = gpu(gpu_id) if gpu_id >= 0 else cpu(0)
@@ -32,16 +48,27 @@ def main(vae_path, video_path, save_path, height=576, width=1024, frame_sample_r
     video = video / 127.5 - 1.0
     video = video[:, :, :frame_end, :, :]
 
+    ts_2 = time.time()
+
     print(f'Shape of input video: {video.shape}')
     latent = vae3d.encode(video).latent_dist.sample()
 
+    ts_3 = time.time()
+
     print(f'Shape of video latent: {latent.shape}')
     results = vae3d.decode(latent).sample
+
+    ts_4 = time.time()
 
     results = rearrange(results, '1 c t h w -> t h w c')
     results = ((torch.clamp(results, -1.0, 1.0) + 1.0) * 127.5).to('cpu', dtype=torch.uint8)
 
     write_video(save_path, results, fps=video_reader.get_avg_fps(), options={'crf': '10'})
+
+    ts_5 = time.time()
+
+    if timeit is True:
+        log_time_benchmark(ts_1, ts_2, ts_3, ts_4, ts_5)
 
 
 if __name__ == '__main__':
